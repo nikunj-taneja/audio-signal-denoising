b8e9ff33feefa78a6369970f6936023632570d2f
underscoreorcus <niktaneja99@gmail.com>
2019-04-23 18:23:44 +0800

    update script and samples

--- script.m
+++ script.m
@@ -1,3 +1,37 @@
-[y, fs] = wavread('samples/original/1.wav');
-t = linspace(0, length(y)/fs, length(y));
-figure; plot(t, y);xlabel("Time"); ylabel("Amplitude");title("Original Audio Signal in Time Domain");N = length(y);f = linspace(0, fs, N);X = abs(fft(y, N));figure; plot(f(1:N/2), X(1:N/2));xlabel("Frequency"); ylabel("Amplitude");title("Original Audio Signal in Frequency Domain");%%for i = 1:N%  if X(i) <= mean_val%    X(i) = 0;%  endif%endfor%%X(50000:N) *= 1/7;X(200:1400) *= 1/1000;figure; plot(f(1:N/2), X(1:N/2));xlabel("Frequency"); ylabel("Amplitude");title("Modified Audio Signal in Frequency Domain");y_prime = ifft(X);t = linspace(0, length(y_prime)/fs, length(y_prime));figure; plot(t, y_prime);xlabel("Time"); ylabel("Amplitude");title("Modified Audio Signal in Time Domain");wavwrite(y_prime, fs, 'samples/clean/1.wav');
+[Y, fs] = wavread('samples/original/sample1.wav');
+% Y is the audio data, fs is the sampling rate
+t = linspace(0, length(Y)/fs, length(Y));
+figure; plot(t, Y);
+xlabel("Time"); ylabel("Amplitude");
+title("Original Audio Signal in Time Domain");
+
+% Apply FFT
+
+N = length(Y);
+f = linspace(0, fs, N);
+X = fft(Y, N);
+figure; plot(f(1:N/2), X(1:N/2));
+xlabel("Frequency"); ylabel("Amplitude");
+title("Original Audio Signal in Frequency Domain");
+
+% Zeroing Out Noise
+
+X(1:10) = 0;
+for i = 1:length(X)
+  if abs(X(i)) < 30
+    X(i) = 0;
+ endif
+endfor
+
+figure; plot(f(1:N/2), X(1:N/2));
+xlabel("Frequency"); ylabel("Amplitude");
+title("Modified Audio Signal in Frequency Domain (Zeroing Out Noise)");
+
+% Apply IFFT
+
+y_prime = ifft(X);
+t = linspace(0, length(y_prime), length(y_prime));
+figure; plot(t, y_prime);
+xlabel("Time"); ylabel("Amplitude");
+title("Modified Audio Signal in Time Domain (Zeroing Out Noise)");
+wavwrite(y_prime, fs, 'samples/clean/sample1.wav');
